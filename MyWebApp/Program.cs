de8c545331aa3e60ca71f09f3d53be5662d110f0
divyeshx07 <divyeshapatel03@gmail.com>
2025-06-25 15:57:20 +0530

    Update Program.cs

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -9,18 +9,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+// 🔥 Add this line before `app.Run()`
+app.Urls.Add("http://0.0.0.0:5000"); // Listens on all interfaces
 
 app.Run();
